6305275f577fe1d6f6cb508c7feb32736a64161c
ChristianoBourguignon <christianombl@gmail.com>
2025-07-22 02:14:58 -0300

    fix: adjust routers in project and new controllers

---
+++ app/Exceptions/httpInvalidException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class httpInvalidException
+{
+
+    /**
+     * @param string $string
+     */
+    public function __construct(string $string)
+    {
+    }
+}
---
+++ app/Exceptions/invalidArgumentsForProductsException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Exceptions;
+
+use http\Exception\RuntimeException;
+
+class invalidArgumentsForProductsException extends RuntimeException
+{
+
+    public function __construct(string $message)
+    {
+        parent::__construct($message);
+    }
+}
