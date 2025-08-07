42ba7d41d0904afe3ae56fd40bb3865ac317e43d
ChristianoBourguignon <christianombl@gmail.com>
2025-08-07 19:08:51 -0300

    fix: removed error log in exception custom

--- app/exceptions/ExceptionCustom.php
+++ app/exceptions/ExceptionCustom.php
@@ -8,12 +8,6 @@ class exceptionCustom extends Exception
 {
     public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
     {
-        if (!is_dir("./logs")) {
-            date_default_timezone_set('America/Sao_Paulo');
-            mkdir("./logs", 0755, true);
-        }
-        error_log("\n".date("Y-m-d H:i:s")."- ". $code . " - ". $message ." ". PHP_EOL ." ". $previous,
-            3, "./logs/log-".date("Y-m-d").".log");
         parent::__construct($message, $code, $previous);
     }
 }
