c56f3c0bc8d45b95166d468f523e631fd49f89f1
ChristianoBourguignon <christianombl@gmail.com>
2025-08-07 19:15:29 -0300

    feat: default timezone

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ require_once "App/routers/router.php";
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+date_default_timezone_set("America/Sao_Paulo");
 
 
 $dotenv = Dotenv::createImmutable(dirname(__FILE__));
